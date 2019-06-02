b9110bc4a3c0fad69623aeab7f750c487f32ad19
rahuljnv <rahulmishrajnv@gmail.com>
2019-06-02 18:20:51 +0530

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -9,14 +9,14 @@
 var1 = {"45*3":555, "55+9":77, "56/6":4}
 var2 = ["+","-","*","/"]
 
-print(var1,var2)
-print("enter two numbers:")
+# print(var1,var2)
+print("Enter two numbers:")
 inp1 = input()
 inp2 = input()
-inch = int(input("Make ur choice:1.+ 2.- 3.* 4./"))
+inch = int(input("Make ur choice:\n1.+\n 2.-\n 3.*\n 4./\n"))
 
 #print(inp1+var2[3]+inp2)
-print(var1.get("55/6"))
+# print(var1.get("55/6"))
 if inch == 1:
     if (inp1+var2[0]+inp2) in var1.keys():
         print(var1.get((inp1+var2[0]+inp2)))
